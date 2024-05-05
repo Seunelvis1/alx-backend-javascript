511c7e735337048ca2e5d64fcd429462e43f7488
Seunelvis1 <adeniranoluwaseunelvis@gmail.com>
2024-05-05 20:15:50 +0100

    correction

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,11 +1,13 @@
 /* eslint-disable no-console */
 import {uploadPhoto, createUser} from './utils';
 
-export default function handleProfileSignup(){
- const promise = [uploadPhoto(), createUser()]
- return	Promise.all(promises).then((resolve)=>{
-console.log(`${resolve[0].body} ${resolve[1].firstName} ${resolve[1].lastName}`);})
-   .catch(() => { console.log('Signup system offline'); });
+export default function handleProfileSignup() {
+  const photo = uploadPhoto();
+  const user = createUser();
 
-	}
+  return Promise.all([photo, user]).then((res) => {
+    console.log(`${res[0].body} ${res[1].firstName} ${res[1].lastName}`);
+  })
+    .catch(() => { console.log('Signup system offline'); });
+}
 
